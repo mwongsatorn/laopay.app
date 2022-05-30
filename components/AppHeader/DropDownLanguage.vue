17e2830e5bbdf4682ee13d931c3240a4f079b876
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-05-30 22:30:02 +0700

    fix: DropDownLanguage component

--- components/AppHeader/DropDownLanguage.vue
+++ components/AppHeader/DropDownLanguage.vue
@@ -8,19 +8,17 @@
     leave-to-class="opacity-0 -translate-y-4"
   >
     <div
-      class="absolute top-full right-0 z-50 divide-y divide-solid rounded-lg border-2 bg-white text-xs shadow-md"
-      @mouseover="$emit('showLanguageList')"
-      @mouseleave="$emit('hideLanguageList')"
+      class="absolute top-[150%] right-0 w-60 z-50 divide-y divide-solid rounded-lg border-2 bg-white text-sm shadow-md"
     >
       <div
         class="cursor-pointer rounded-lg px-8 py-2 text-center hover:bg-slate-100"
       >
-        <span>Lao</span>
+        <span class="font-noto-sans-lao">ພາສາລາວ</span>
       </div>
       <div
         class="cursor-pointer rounded-lg px-8 py-2 text-center hover:bg-slate-100"
       >
-        <span>Thai</span>
+        <span>ภาษาไทย</span>
       </div>
       <div
         class="cursor-pointer rounded-lg px-8 py-2 text-center hover:bg-slate-100"
