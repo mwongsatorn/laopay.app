94058f9fe662a3fab573cd52ebbfbfdd4ec04611
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-05-30 14:23:44 +0700

    feat: about us page

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mx-auto max-w-7xl px-6 py-12">
+  <div class="mx-auto max-w-7xl px-4 py-12">
     <h1 class="font-noto-sans-lao text-2xl font-bold sm:text-3xl lg:text-4xl">
       ຕິດຕໍ່ພວກເຮົາ
     </h1>
-    <div class="grid grid-cols-1 gap-6 pt-8 sm:grid-cols-2">
+    <div class="grid grid-cols-1 gap-6 pt-8 md:grid-cols-2">
       <div class="rounded-lg border-2 shadow-xl">
         <div class="h-24 rounded-t-lg bg-gray-200 p-4">
           <img src="~/assets/images/laopay-logo.png" class="h-full" alt="" />
@@ -24,8 +24,8 @@
         </div>
       </div>
       <div class="rounded-lg border-2 font-roboto shadow-xl">
-        <div class="h-24 rounded-t-lg bg-gray-200 p-4">
-          <img src="~/assets/images/r2c-logo-half.png" class="h-full" alt="" />
+        <div class="flex h-24 items-center bg-gray-200 p-4">
+          <img src="~/assets/images/r2c-logo.png" class="h-12" alt="" />
         </div>
         <div class="space-y-4 p-4">
           <p>
