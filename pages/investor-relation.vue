d22c167f8aed62f8d285e35b131c692991955330
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-05-30 23:25:53 +0700

    feat: investor relation page

---
+++ pages/investor-relation.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mx-auto max-w-7xl px-4 py-12">
+    <h1 class="font-noto-sans-lao text-2xl font-bold sm:text-3xl lg:text-4xl">
+      ກ່ຽວກັບການລົງທຶນ
+    </h1>
+    <div class="w-full pt-8">
+      <img src="~/assets/images/laopay-teaser-040322-01.png" alt="" />
+    </div>
+  </div>
+</template>
