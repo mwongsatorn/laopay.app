e8a84cc2dba4fa37829e329ead694a726d274fab
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-05-30 23:45:45 +0700

    feat: favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'laopay.app',
+    title: 'LaoPay Super App',
     htmlAttrs: {
       lang: 'en',
     },
