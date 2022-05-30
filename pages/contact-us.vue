24e09da903b65cef968f05a7df62d47b6bb2c1c4
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-05-30 11:37:13 +0700

    feat: contact us page

---
+++ pages/contact-us.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="mx-auto max-w-7xl px-6 py-12">
+    <h1 class="font-noto-sans-lao text-2xl font-bold sm:text-3xl lg:text-4xl">
+      ຕິດຕໍ່ພວກເຮົາ
+    </h1>
+    <div class="grid grid-cols-1 gap-6 pt-8 sm:grid-cols-2">
+      <div class="rounded-lg border-2 shadow-xl">
+        <div class="h-24 rounded-t-lg bg-gray-200 p-4">
+          <img src="~/assets/images/laopay-logo.png" class="h-full" alt="" />
+        </div>
+        <div class="space-y-4 p-4">
+          <p class="font-roboto">
+            <span class="font-bold">LaoPay Vientiane Sole Co., Ltd</span>.
+            <br />
+            Buengkayong Village, Sisattanak, Vientiane Capital, Lao PDR No. 560
+            Unit 29
+          </p>
+          <p class="font-noto-sans-lao">
+            <span class="font-bold">ບໍລິສັດ ລາວເພ ວຽງຈັນ ຈໍາກັດຜູ້ດຽວ</span>
+            <br />
+            ບ้ານບຶງຂະຫຍອງ ເມືອງສີສັດຕະນາກ ແຂວງນະຄອນຫຼວງວຽງຈັນ ເຮືອນເລກທ่ີ 560
+            ໜ່ວຍ 29
+          </p>
+        </div>
+      </div>
+      <div class="rounded-lg border-2 font-roboto shadow-xl">
+        <div class="h-24 rounded-t-lg bg-gray-200 p-4">
+          <img src="~/assets/images/r2c-logo-half.png" class="h-full" alt="" />
+        </div>
+        <div class="space-y-4 p-4">
+          <p>
+            <span class="font-bold">Ready To Connect (UK) Ltd.</span> <br />
+            Kemp House 152-160 City Road London EC1V 2NX United Kingdom
+          </p>
+          <p>
+            <span class="font-bold">Ready To Connect (Laos) Co., Ltd.</span>
+            <br />
+            Thong Pong Road, Thong Pong Village, Sikottabong, Vientiane Capital,
+            Lao PDR
+          </p>
+          <p>
+            <span class="font-bold">Ready To Connect (Thailand) Co., Ltd.</span>
+            <br />
+            75/9 Ocean Tower 2 Building, Room No.4, 14th Floor, Soi Sukhumvit 19
+            (Vadhana), Sukhumvit Road, Khlong Toei Nuea, Vadhana, Bangkok 10110
+            Thailand
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
