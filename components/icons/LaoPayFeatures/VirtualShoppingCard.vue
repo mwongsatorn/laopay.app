5fa8ca71da97b68b92390ae8cb3d8c9d62e8dadb
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-06-07 10:13:35 +0700

    feat: product and service page

---
+++ components/icons/LaoPayFeatures/VirtualShoppingCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="75"
+    height="75"
+    viewBox="0 0 75 75"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="75" height="75" rx="10" fill="#171045" />
+    <path
+      d="M17 57C15.625 57 14.4483 56.5108 13.47 55.5325C12.49 54.5525 12 53.375 12 52V22C12 20.625 12.49 19.4483 13.47 18.47C14.4483 17.49 15.625 17 17 17H57C58.375 17 59.5525 17.49 60.5325 18.47C61.5108 19.4483 62 20.625 62 22V52C62 53.375 61.5108 54.5525 60.5325 55.5325C59.5525 56.5108 58.375 57 57 57H17ZM17 27H57V22H17V27ZM17 37V52H57V37H17ZM17 52V22V52Z"
+      fill="white"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'VirtualShoppingCard',
+}
+</script>
+
