eaac07d343ee43252d33c91978c0fe01d28be846
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-06-22 12:44:20 +0700

    fix(style): layout on highlight features page

--- pages/products-and-services/highlight-features.vue
+++ pages/products-and-services/highlight-features.vue
@@ -6,7 +6,7 @@
           bannerSuffix +
           '.jpg')})`,
       }"
-      class="relative -mt-20 h-screen min-h-[45rem] bg-cover bg-center sm:-mt-24"
+      class="-mt-20 h-screen min-h-[45rem] bg-cover bg-center md:-mt-24"
     >
       <div
         class="relative top-1/3 -translate-y-1/2 space-y-6 px-6 font-noto-sans-lao md:top-1/2 md:px-8 md:pt-0"
@@ -24,21 +24,21 @@
       </div>
     </div>
 
-    <div class="mx-auto max-w-7xl px-4 py-20">
+    <div class="px-4 py-20">
       <h1 class="font-noto-sans-lao text-2xl font-bold sm:text-3xl lg:text-4xl">
         ຄຸນສົມບັດ ລາວເພ້
       </h1>
-      <div
-        class="grid grid-cols-1 gap-8 pt-8 font-noto-sans-lao sm:grid-cols-2 lg:grid-cols-[350px_1fr_350px]"
-      >
-        <div class="flex flex-col justify-around space-y-8">
+      <div class="flex flex-wrap gap-8 pt-8 font-noto-sans-lao">
+        <div
+          class="flex max-w-fit flex-[1_0_320px] flex-col justify-around space-y-8 sm:max-w-full"
+        >
           <div class="flex items-center gap-x-4">
             <div class="shrink-0">
               <IconQRPayment></IconQRPayment>
             </div>
             <div class="flex flex-col">
               <span class="text-xl font-bold">ຄິວອາ</span>
-              <p>ສະແກນເພື່ອທຳການຊຳລະ</p>
+              <p>ສະແກນເພື່ອຮັບ ແລະ ສະແກນເພື່ອຈ່າຍເງິນ</p>
             </div>
           </div>
 
@@ -49,7 +49,7 @@
             <div class="flex flex-col">
               <span class="text-xl font-bold">ຕື່ມເງິນເຂົ້າກະເປົາເງິນ</span>
               <p>
-                ຕື່ມເງິນເຂົ້າກະເປົາເງິນ ລາວເພ້ ຜ່ານບັນຊີທະນາຄານ, ຕູ້ຕື່ມເງິນ,
+                ຕື່ມເງິນຜ່ານບັນຊີທະນາຄານ, ຕູ້ຕື່ມເງິນ,
                 ແອັບຕົວແທນໂດຍຜ່ານຕົວແທນລາວເພ້
               </p>
             </div>
@@ -62,7 +62,8 @@
             <div class="flex flex-col">
               <span class="text-xl font-bold">ການໂອນເງິນລະຫວ່າງປະເທດ </span>
               <p>
-                ໂອນໄດ້ທຸກທີ່ທົ່ວໂລກຫລາຍກວ່າ 150 ສະກຸນເງິນຜ່ານທະນາຄານພັນທະມິດ
+                ສາມາດໂອນໄດ້ທັງພາຍໃນປະເທດ ແລະ ຕ່າງປະເທດໄດ້ທຸກທີ່ທົ່ວໂລກ ຫຼາຍກວ່າ
+                150 ປະເທດ ຜ່ານທະນາຄານທີ່ເປັນພັນທະມິດ
               </p>
             </div>
           </div>
@@ -78,15 +79,9 @@
           </div>
         </div>
 
-        <div class="hidden lg:block">
-          <img
-            class="mx-auto max-h-[550px]"
-            src="~/assets/images/products-and-services/app-homepage.png"
-            alt=""
-          />
-        </div>
-
-        <div class="flex flex-col justify-around space-y-8">
+        <div
+          class="flex max-w-fit flex-[1_0_320px] flex-col justify-around space-y-8 sm:max-w-full"
+        >
           <div class="flex items-center gap-x-4">
             <div class="shrink-0">
               <IconPayBill></IconPayBill>
@@ -113,10 +108,7 @@
             </div>
             <div class="flex flex-col">
               <span class="text-xl font-bold">ຊ໊ອບປິ້ງ </span>
-              <p>
-                ແພັດຟອມຊື້ຂາຍອອນລາຍທີ່ຄົບຈົບໃນແອັບດຽວ. ເປັນຫົນທາງທີ່ສະດວກໃນການ
-                ຊື້-ຂາຍ ແລະ ຈ່າຍຜ່ານ "ລາວເພ້" ໄດ້ອີກດ້ວຍ
-              </p>
+              <p>ແພັດຟອມຊື້ຂາຍອອນລາຍທີ່ຄົບຈົບໃນແອັບດຽວ</p>
             </div>
           </div>
 
@@ -127,9 +119,7 @@
             <div class="flex flex-col">
               <span class="text-xl font-bold">ໂປຣໂມຊັ້ນ & ຂອງລາງວັນ </span>
               <p>
-                ສຳລັບທຸກໆການເຮັດທຸລະກຳຜ່ານລາວເພ້ແອັບພິເຄຊັ້ນ,
-                ຜູ້ໃຊ້ງານຈະໄດ້ຮັບຄະແນນເພື່ອຮັບສ່ວນຫຼຸດ ຫຼື
-                ແລກຂອງລາງວັນຈາກຮ້ານຄ້າທີ່ມີສ່ວນຮ່ວມ
+                ຮັບຄະແນນເພື່ອຮັບສ່ວນຫຼຸດ ຫຼື ແລກຂອງລາງວັນຈາກຮ້ານຄ້າທີ່ມີສ່ວນຮ່ວມ
               </p>
             </div>
           </div>
@@ -137,45 +127,47 @@
       </div>
     </div>
 
-    <div class="bg-blue-50">
-      <div class="mx-auto max-w-7xl px-4 py-20">
-        <h1 class="font-roboto text-2xl font-bold sm:text-3xl lg:text-4xl">
-          1 Click 2 Connect ™
-        </h1>
-        <h2
-          class="pt-4 text-center font-noto-sans-lao text-lg sm:text-xl lg:text-2xl"
-        >
-          2 ຂັ້ນຕອນງ່າຍໆໃນການເຊື່ອມຕໍ່ ຟຣີ ວາຍຟາຍ ໃນແອັບ ລາວເພ້
-        </h2>
-        <div class="grid grid-cols-1 gap-8 pt-8 sm:grid-cols-2">
-          <div class="flex flex-col items-center justify-between space-y-4">
-            <div class="flex space-x-4">
-              <span
-                class="flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-primary-blue font-bold text-white"
-              >
-                1
-              </span>
-              <p class="text-center font-noto-sans-lao">
-                ເປີດແອັບ “ລາວເພ້" ແລະ ກົດປຸ່ມ “ຟຣີ ວາຍຟາຍ"
-              </p>
-            </div>
+    <div class="bg-blue-50 px-4 py-20">
+      <h1 class="font-roboto text-2xl font-bold sm:text-3xl lg:text-4xl">
+        1 Click 2 Connect ™
+      </h1>
+      <h2
+        class="pt-4 text-center font-noto-sans-lao text-lg sm:text-xl lg:text-2xl"
+      >
+        2 ຂັ້ນຕອນງ່າຍໆໃນການເຊື່ອມຕໍ່ ຟຣີ ວາຍຟາຍ ໃນແອັບ ລາວເພ້
+      </h2>
+      <div class="flex flex-wrap justify-around gap-8 pt-8">
+        <div class="flex max-w-fit flex-[1_0_320px] flex-col space-y-4">
+          <div class="flex space-x-4">
+            <span
+              class="flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-primary-blue font-bold text-white"
+            >
+              1
+            </span>
+            <p class="text-center font-noto-sans-lao">
+              ເປີດແອັບ “ລາວເພ້" ແລະ ກົດປຸ່ມ “ຟຣີ ວາຍຟາຍ"
+            </p>
+          </div>
+          <div class="self-center">
             <img
               class="max-h-[400px]"
               src="~/assets/images/products-and-services/step1_connect-wifi.png"
               alt=""
             />
           </div>
-          <div class="flex flex-col items-center justify-between space-y-4">
-            <div class="flex space-x-4">
-              <span
-                class="flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-primary-blue font-bold text-white"
-              >
-                2
-              </span>
-              <p class="text-center font-noto-sans-lao">
-                ເບິ່ງໂຄສະນາ 7-15 ວິນາທີ ຟຣີ ວາຍຟາຍ ເຊື່ອມຕໍ່ສຳເລັດ
-              </p>
-            </div>
+        </div>
+        <div class="flex max-w-fit flex-[1_0_320px] flex-col space-y-4">
+          <div class="flex space-x-4">
+            <span
+              class="flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-primary-blue font-bold text-white"
+            >
+              2
+            </span>
+            <p class="text-center font-noto-sans-lao">
+              ເບິ່ງໂຄສະນາ 7-15 ວິນາທີ ຟຣີ ວາຍຟາຍ ເຊື່ອມຕໍ່ສຳເລັດ
+            </p>
+          </div>
+          <div class="self-center">
             <img
               class="max-h-[400px]"
               src="~/assets/images/products-and-services/step2_connect-wifi.png"
