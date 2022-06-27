7fce7fe4eb1b6f4d97b2c31c9f82dfdf223d9c50
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-06-27 11:55:35 +0700

    fix: sidebar menu

--- components/ProductsAndServicesPage/SideBarMenu.vue
+++ components/ProductsAndServicesPage/SideBarMenu.vue
@@ -1,67 +1,71 @@
 <template>
   <div
-    class="fixed top-20 bottom-0 z-10 overflow-y-scroll whitespace-nowrap bg-white transition-all duration-300 md:top-24"
+    class="fixed top-20 bottom-0 z-10 whitespace-nowrap bg-white transition-all duration-300 md:top-24"
     :class="[showSideBar ? 'w-[300px]' : 'w-0']"
   >
-    <div
-      class="flex items-center justify-between bg-neutral-900 px-4 py-4 font-bold text-white"
-    >
-      <span>ເມນູ</span>
-      <button @click="$emit('closeSideBar')">
-        <IconArrowCollapse class="h-6 w-6 rotate-180"> </IconArrowCollapse>
-      </button>
+    <div class="h-full overflow-y-scroll">
+      <nav class="flex flex-col px-4 py-4">
+        <ul>
+          <NuxtLink
+            v-for="menu in menuList"
+            :key="menu.routePath"
+            v-slot="{ href, navigate, isActive, isExactActive }"
+            :to="menu.routePath"
+            custom
+          >
+            <li v-if="!menu.subMenu" @click="$emit('onClickMenu')">
+              <a
+                class="block py-2"
+                :class="[isExactActive ? 'font-bold text-primary-red' : '']"
+                :href="href"
+                @click="navigate"
+                >{{ menu.routeName }}</a
+              >
+            </li>
+
+            <li v-else class="py-2">
+              <span :class="[isActive ? 'font-bold text-primary-red' : '']">
+                {{ menu.routeName }}
+              </span>
+              <ul class="mt-4 text-sm">
+                <NuxtLink
+                  v-for="subMenu in menu.subMenu"
+                  :key="subMenu.routePath"
+                  v-slot="{ href, navigate, isExactActive }"
+                  :to="subMenu.routePath"
+                  custom
+                >
+                  <li @click="$emit('onClickMenu')">
+                    <a
+                      class="block border-l-[3px] py-1.5 pl-4"
+                      :class="[
+                        isExactActive
+                          ? 'border-primary-red font-bold text-primary-red'
+                          : 'hover:border-primary-blue hover:text-primary-blue',
+                      ]"
+                      :href="href"
+                      @click="navigate"
+                      >{{ subMenu.routeName }}</a
+                    >
+                  </li>
+                </NuxtLink>
+              </ul>
+            </li>
+          </NuxtLink>
+        </ul>
+      </nav>
     </div>
 
-    <nav class="flex flex-col px-4 py-4">
-      <ul>
-        <NuxtLink
-          v-for="menu in menuList"
-          :key="menu.routePath"
-          v-slot="{ href, navigate, isActive, isExactActive }"
-          :to="menu.routePath"
-          custom
-        >
-          <li v-if="!menu.subMenu" @click="$emit('toggleSideBar')">
-            <a
-              class="block py-2"
-              :class="[isExactActive ? 'font-bold text-primary-red' : '']"
-              :href="href"
-              @click="navigate"
-              >{{ menu.routeName }}</a
-            >
-          </li>
-
-          <li v-else class="py-2">
-            <span :class="[isActive ? 'font-bold text-primary-red' : '']">
-              {{ menu.routeName }}
-            </span>
-            <ul class="mt-4 text-sm">
-              <NuxtLink
-                v-for="subMenu in menu.subMenu"
-                :key="subMenu.routePath"
-                v-slot="{ href, navigate, isExactActive }"
-                :to="subMenu.routePath"
-                custom
-              >
-                <li @click="$emit('toggleSideBar')">
-                  <a
-                    class="block border-l-[3px] py-1.5 pl-4"
-                    :class="[
-                      isExactActive
-                        ? 'border-primary-red font-bold text-primary-red'
-                        : 'hover:border-primary-blue hover:text-primary-blue',
-                    ]"
-                    :href="href"
-                    @click="navigate"
-                    >{{ subMenu.routeName }}</a
-                  >
-                </li>
-              </NuxtLink>
-            </ul>
-          </li>
-        </NuxtLink>
-      </ul>
-    </nav>
+    <button
+      class="absolute top-1/2 left-full flex h-16 w-8 -translate-y-1/2 items-center justify-center rounded-r-3xl bg-white"
+      @click="$emit('toggleSideBar')"
+    >
+      <IconArrowCollapse
+        :class="[showSideBar ? 'rotate-180' : '']"
+        class="h-5 w-5"
+      >
+      </IconArrowCollapse>
+    </button>
   </div>
 </template>
 
@@ -69,7 +73,6 @@
 export default {
   props: {
     showSideBar: {
-      default: null,
       type: Boolean,
     },
   },
@@ -81,27 +84,31 @@ export default {
           routeName: 'ບໍລິການຍອດນິຍົມ',
         },
         {
-          routePath: '/products-and-services/how-to-use',
+          routePath: '/products-and-services/app-guide',
           routeName: 'ວິທີການໃຊ້ງານ',
           subMenu: [
             {
-              routePath: '/products-and-services/how-to-use/sign-up',
+              routePath: '/products-and-services/app-guide/sign-up',
               routeName: 'ລົງທະບຽນ',
             },
             {
-              routePath: '/products-and-services/how-to-use/login',
+              routePath: '/products-and-services/app-guide/log-in',
               routeName: 'ເຂົ້າສູ່ລະບົບ',
             },
             {
-              routePath: '/products-and-services/how-to-use/forget-password',
+              routePath: '/products-and-services/app-guide/forgot-password',
               routeName: 'ລືມລະຫັດຜ່ານ',
             },
             {
-              routePath: '/products-and-services/how-to-use/pay',
+              routePath: '/products-and-services/app-guide/pay',
               routeName: 'ຈ່າຍ',
             },
             {
-              routePath: '/products-and-services/how-to-use/wallet-top-up',
+              routePath: '/products-and-services/app-guide/collect',
+              routeName: 'ຮັບເງິນ',
+            },
+            {
+              routePath: '/products-and-services/app-guide/wallet-top-up',
               routeName: 'ການຕື່ມເງິນເຂົ້າກະເປົາເງິນ',
             },
           ],
--- pages/products-and-services.vue
+++ pages/products-and-services.vue
@@ -2,8 +2,8 @@
   <div class="mx-auto max-w-7xl overflow-hidden">
     <SideBarMenu
       :show-side-bar="showSideBar"
-      @toggleSideBar="toggleSideBar"
-      @closeSideBar="showSideBar = false"
+      @toggleSideBar="showSideBar = !showSideBar"
+      @onClickMenu="onClickMenu"
     >
     </SideBarMenu>
     <NuxtChild
@@ -13,13 +13,6 @@
       ]"
     >
     </NuxtChild>
-    <button
-      :class="[showSideBar ? 'hidden' : '']"
-      class="fixed top-24 ml-4 flex h-12 w-12 items-center justify-center rounded-full border-2 bg-white shadow-md md:top-28"
-      @click="showSideBar = true"
-    >
-      <IconArrowCollapse class="h-6 w-6"> </IconArrowCollapse>
-    </button>
   </div>
 </template>
 
@@ -27,14 +20,15 @@
 export default {
   data() {
     return {
-      showSideBar: true,
+      showSideBar: null,
     }
   },
   mounted() {
-    this.toggleSideBar()
+    if (window.innerWidth <= 1024) this.showSideBar = false
+    else this.showSideBar = true
   },
   methods: {
-    toggleSideBar() {
+    onClickMenu() {
       if (window.innerWidth <= 1024) this.showSideBar = false
       else this.showSideBar = true
     },
