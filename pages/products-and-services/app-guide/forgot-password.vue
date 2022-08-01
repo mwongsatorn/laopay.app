6faff9f94b8f58c65118e6e1d97260f75c66f246
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-08-01 14:53:31 +0700

    feat: english locale for app-guide/forgot-password page

--- pages/products-and-services/app-guide/forgot-password.vue
+++ pages/products-and-services/app-guide/forgot-password.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-primary-blue py-12 px-4 text-white sm:px-8 lg:px-12">
-    <h1 class="text-2xl font-bold sm:text-3xl lg:text-4xl">ລືມລະຫັດຜ່ານ</h1>
+    <h1 class="text-2xl font-bold sm:text-3xl lg:text-4xl">
+      {{ $t('title') }}
+    </h1>
     <div class="flex flex-nowrap overflow-x-auto py-8">
       <div
         v-for="(guide, index) in forgotPasswordGuide"
@@ -20,12 +22,39 @@
           class="max-h-[450px]"
         />
         <div>
-          <p class="text-center">{{ guide.description }}</p>
+          <p class="text-center">{{ $t(guide.description) }}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<i18n>
+{
+  "lo": {
+    "title": "ລືມລະຫັດຜ່ານ",
+    "step1": "ເລືອກປຸ່ມ \"ເຂົ້າສູ່ລະບົບ\" (Log in)",
+    "step2": "ກົດເລືອກປຸ່ມ \"ລືມລະຫັດຜ່ານ ?\" (Forgot Password ?)",
+    "step3": "ໃສ່ເບີໂທລະສັບ (Mobile  Number)",
+    "step4": "ລະຫັດຢືນຢັນຕົວຕົນ (One Time Password: OTP) ຈະຖືກສົ່ງໄປທີ່ເບີໂທລະສັບຂອງຜູ້ໃຊ້ບໍລິການທີ່ໄດ້ຂຽນໄວ້",
+    "step5": "ຫ້ຜູ້ໃຊ້ບໍລິການໃສ່ໂຕເລກດັ່ງກ່າວລົງໃນຊ່ອງທີ່ກໍານົດ",
+    "step6": "ຕັ້ງລະຫັດຜ່ານໃໝ່ ໃນປ່ອງລະຫັດຜ່ານ (Password)",
+    "step7": "ຢືນຢັນ ລະຫັດຜ່ານອີກຄັ້ງ",
+    "step8": "ເຂົ້າສູ່ໜ້າຈໍຫລັກຂອງແອັບພິເຄຊັ້ນ LaoPay"
+  },
+  "en": {
+    "title": "Forgot Password",
+    "step1": "Click \"Login\" botton",
+    "step2": "Click \"Forget Password\" button",
+    "step3": "Fill in registered mobile number",
+    "step4": "OTP code (One Time Password) will send to registered mobile number",
+    "step5": "Input OTP code",
+    "step6": "Set new password",
+    "step7": "Confirm new password",
+    "step8": "Visit LaoPay Homepage"
+  }
+}
+</i18n>
 
 <script>
 export default {
@@ -34,36 +63,35 @@ export default {
       forgotPasswordGuide: [
         {
           image: '/welcome-page.jpg',
-          description: 'ເລືອກປຸ່ມ “ເຂົ້າສູ່ລະບົບ (Log in)”',
+          description: 'step1',
         },
         {
           image: '/forgot-password/forgot-password-step-2.jpg',
-          description: 'ກົດເລືອກປຸ່ມ “ລືມລະຫັດຜ່ານ? (Forgot Password?)',
+          description: 'step2',
         },
         {
           image: '/forgot-password/forgot-password-step-3.jpg',
-          description: 'ໃສ່ເບີໂທລະສັບ (Mobile  Number)',
+          description: 'step3',
         },
         {
           image: '/forgot-password/forgot-password-step-4.jpg',
-          description:
-            'ລະຫັດຢືນຢັນຕົວຕົນ (One Time Password: OTP) ຈະຖືກສົ່ງໄປທີ່ເບີໂທລະສັບຂອງຜູ້ໃຊ້ບໍລິການທີ່ໄດ້ຂຽນໄວ້',
+          description: 'step4',
         },
         {
           image: '/forgot-password/forgot-password-step-5.jpg',
-          description: 'ຫ້ຜູ້ໃຊ້ບໍລິການໃສ່ໂຕເລກດັ່ງກ່າວລົງໃນຊ່ອງທີ່ກໍານົດ',
+          description: 'step5',
         },
         {
           image: '/forgot-password/forgot-password-step-6.jpg',
-          description: 'ຕັ້ງລະຫັດຜ່ານໃໝ່ ໃນປ່ອງລະຫັດຜ່ານ (Password)',
+          description: 'step6',
         },
         {
           image: '/forgot-password/forgot-password-step-7.jpg',
-          description: 'ຢືນຢັນ ລະຫັດຜ່ານອີກຄັ້ງ',
+          description: 'step7',
         },
         {
           image: '/homepage.jpg',
-          description: 'ເຂົ້າສູ່ໜ້າຈໍຫລັກຂອງແອັບພິເຄຊັ້ນ LaoPay',
+          description: 'step8',
         },
       ],
     }
