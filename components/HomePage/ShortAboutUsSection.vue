eb8ccb8e1e65b0a9ab1f031a2ebf241a7fdabb76
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-08-02 10:40:22 +0700

    fix: rework short about us section on homepage

--- components/HomePage/ShortAboutUsSection.vue
+++ components/HomePage/ShortAboutUsSection.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="short-about-us-section" class="bg-stone-400">
+  <div
+    id="short-about-us-section"
+    class="bg-[url('~/assets/images/index/sau-bg-mobile.jpg')] bg-cover bg-[position:0%_85%] sm:bg-[url('~/assets/images/index/sau-bg-tablet.jpg')] lg:bg-[url('~/assets/images/index/sau-bg-desktop.jpg')]"
+  >
     <div
-      class="mx-auto grid min-h-[640px] max-w-xl grid-cols-1 place-content-center gap-8 px-6 py-12 md:max-w-7xl md:grid-cols-2"
+      class="mx-auto flex max-w-7xl text-white sm:justify-center lg:justify-start"
     >
       <div
-        class="h-[280px] w-full bg-[url('~/assets/images/office.jpg')] bg-cover bg-center sm:h-[360px]"
-      />
+        class="flex h-screen min-h-[640px] flex-col space-y-8 py-12 px-6 sm:max-w-xl lg:max-w-[400px] lg:justify-center"
+      >
+        <h1 class="text-3xl font-bold text-sky-200 sm:text-4xl lg:text-5xl">
+          {{ $t('title') }}
+        </h1>
+        <h2 class="font-roboto text-lg font-bold sm:text-xl lg:text-2xl">
+          LaoPay Vientiane Sole Co., Ltd.
+        </h2>
 
-      <div class="flex flex-col justify-between space-y-8">
-        <div class="space-y-4">
-          <h1 class="text-2xl font-bold sm:text-3xl lg:text-4xl">
-            {{ $t('title') }}
-          </h1>
-          <h2 class="font-roboto text-lg font-bold sm:text-xl lg:text-2xl">
-            LaoPay Vientiane Sole Co., Ltd.
-          </h2>
-          <p class="">
-            {{ $t('description') }}
-          </p>
-        </div>
-
-        <div class="flex">
-          <LinkButton :to="localePath('/about-us')">
-            {{ $t('button_text') }}
-          </LinkButton>
-        </div>
+        <p class="text-base lg:text-lg">{{ $t('description') }}</p>
+        <LinkButton to="/about-us" class="w-fit text-black">{{
+          $t('buttonText')
+        }}</LinkButton>
       </div>
     </div>
   </div>
@@ -35,12 +30,12 @@
   "lo": {
     "title": "ກ່ຽວກັບພວກເຮົາ",
     "description": "ບໍລິສັດ ລາວເພ້ ວຽງຈັນ ຈຳກັດຜູ້ດຽວ ກໍ່ຕັ້ງຂຶ້ນໃນປີ 2019 ໂດຍເກີດຈາກຄວາມຮ່ວມມືກັນລະຫວ່າງ ບໍລິສັດ ເຣດດີ້ ທູ ຄອນເນັກ (ລາວ) ຈຳກັດ ແລະ ບໍລິສັດ ເອກະໄຊໂຊກ ອະສັງຫາລິມະສັບ ຈຳກັດຜູ້ດຽວ ດຳເນີນທຸລະກິດອີວໍເລັດ ພັດທະນາແອັບພິເຄຊັ້ນໂດຍໃຊ້ຊື່ວ່າ: ລາເພ້ (LaoPay) ແລະເປັນຜູ້ໃຫ້ບໍລິການ ອອນລາຍເພເມັ້ນ",
-    "button_text": "ອ່ານເພີ່ມຕື່ມ"
+    "buttonText": "ອ່ານເພີ່ມຕື່ມ"
   },
   "en": {
     "title": "About Us",
     "description": "LaoPay Vientiane Sole Co., Ltd. is established since 2019 which is in collaboration between Ready To Connect (Laos) Co., Ltd. and Akasaysok Real Estate Sole Co., Ltd. To develop e-Wallet mobile application nationwide namely “LaoPay” and online payment service provider.",
-    "button_text": "Read More"
+    "buttonText": "Read More"
   }
 }
 </i18n>
